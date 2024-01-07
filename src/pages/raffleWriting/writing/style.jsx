cf33b3af24ca76da7c0342179844d8415c4ed3a3
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-01-08 03:10:28 +0900

    Feat: 페이지별 path 연결

--- src/pages/raffleWriting/writing/style.jsx
+++ src/pages/raffleWriting/writing/style.jsx
@@ -2,10 +2,4 @@ import { styled } from 'styled-components';
 
 export const WritingWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 90vh;
-  width: 100%;
-  background-color: #f5f5f5;
 `;
--- src/router.jsx
+++ src/router.jsx
@@ -6,6 +6,9 @@ import WinningConfirmBox from './pages/winningConfirm/box/Box.jsx';
 import RaffleBox from './pages/raffle/box/Box.jsx';
 import Creator from './pages/creator/Creator.jsx';
 import Setting from './pages/setting/Setting.jsx';
+import RaffleWriting from './pages/raffleWriting/writing/Writing.jsx';
+import RaffleWritingComplete from './pages/raffleWriting/complete/Complete.jsx';
+import RaffleDetail from './pages/raffle/detail/Detail.jsx';
 
 const router = createBrowserRouter([
   {
@@ -31,6 +34,18 @@ const router = createBrowserRouter([
       {
         path: '/creator',
         element: <Creator />,
+      },
+      {
+        path: '/raffleWriting',
+        element: <RaffleWriting />,
+      },
+      {
+        path: '/raffleWriting/complete',
+        element: <RaffleWritingComplete />,
+      },
+      {
+        path: '/raffle/detail',
+        element: <RaffleDetail />,
       },
     ],
   },
