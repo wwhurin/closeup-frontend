61ac770c3ff06f0d34536288517b300925fd44bb
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-01-09 01:30:06 +0900

    Feat: 래플 디테일 신청 페이지 컴포넌트화

---
+++ src/components/raffleApplyment/detail/style.jsx
@@ -0,0 +1,117 @@
+import { styled } from 'styled-components';
+
+export const DetailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const DetailTitle = styled.div`
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 20px;
+  margin-top: 20px;
+  margin-bottom: 5px;
+`;
+
+export const DetailDate = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2px 50px;
+  background-color: #5d00ff;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 20px;
+  border-radius: 40px;
+  text-align: center;
+`;
+
+export const DetailContentWrapper = styled.div`
+  display: flex;
+  min-width: 100%;
+  padding: 0px 10px;
+  min-height: 321px;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+  border-radius: 20px;
+  border: 1px solid #8148f6;
+  box-shadow: 0px 0px 4px 0px #000;
+  backdrop-filter: blur(5px);
+`;
+
+export const DetailContentImg = styled.img`
+  width: 247px;
+  height: 128px;
+  object-fit: cover;
+  border-radius: 10px;
+  margin-top: 16px;
+`;
+
+export const DetailContentHeader = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin-top: 12px;
+`;
+export const DetailContentPrice = styled.div`
+  color: #dc8ae1;
+  width: 100%;
+  display: flex;
+  margin-left: 20px;
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 20px;
+`;
+export const DetailContentProfile = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  margin-right: 20px;
+  align-items: center;
+`;
+export const DetailContentProfileImg = styled.img`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  margin-right: 5px;
+`;
+export const DetailContentProfileName = styled.div`
+  color: #fff;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 20px;
+`;
+export const DetailContent = styled.div`
+  color: #fff;
+  font-size: 9px;
+  font-weight: 400;
+  line-height: 20px;
+  margin: 10px 0px;
+  display: flex;
+  width: 234px;
+`;
+export const ApplyButton = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 0px;
+  width: 273px;
+  height: 33px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 40px;
+  background: linear-gradient(213deg, #7d3af1 32.82%, #dc8ae1 84.65%);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(2px);
+  color: #fff;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 20px;
+  margin-top: 20px;
+  margin-bottom: 16px;
+  cursor: pointer;
+`;
--- src/pages/raffle/detail/Detail.jsx
+++ src/pages/raffle/detail/Detail.jsx
@@ -1,34 +1,25 @@
 import React from 'react';
 import * as S from './style';
 import Header from '../../../components/raffleApplyment/header/Header';
-import ProfileImg from '../../../assets/images/raffleApplyment/profile.png';
+import RaffleDetail from '../../../components/raffleApplyment/detail/Detail';
 import raffleImg from '../../../assets/images/raffleApplyment/raffleImg.png';
+import ProfileImg from '../../../assets/images/raffleApplyment/profile.png';
 
 function Detail() {
   return (
     <>
       <Header title={'래플'} />
-      <S.DetailWrapper>
-        <S.DetailTitle>영상 이벤트</S.DetailTitle>
-        <S.DetailDate>2023.04.25 ~ 2023.10.27</S.DetailDate>
-        <S.DetailContentWrapper>
-          <S.DetailContentImg src={raffleImg} />
-          <S.DetailContentHeader>
-            <S.DetailContentPrice>응모 금액 : 1000원 </S.DetailContentPrice>
-            <S.DetailContentProfile>
-              <S.DetailContentProfileImg src={ProfileImg} />
-              <S.DetailContentProfileName>도티</S.DetailContentProfileName>
-            </S.DetailContentProfile>
-          </S.DetailContentHeader>
-          <S.DetailContent>
-            여러분 안녕하세요 :) 도티입니다 무더운 여름 잘 보내고 계신가요?
-            어쩌구 저쩌구 skdflskdjglksd;fsk;glflgkjsldfjgn;ldfkjgm;lkfjgdl;kf
-            여러분 안녕하세요 :) 도티입니다 무더운 여름 잘 보내고 계신가요?
-            어쩌구 저쩌구 skdflskdjglksd;fsk;glflgkjsldfjgn;ldfkjgm;lkfjgdl;kf
-          </S.DetailContent>
-        </S.DetailContentWrapper>
-        <S.ApplyButton>신청하기</S.ApplyButton>
-      </S.DetailWrapper>
+      <RaffleDetail
+        title={'영상 이벤트'}
+        date={'2023.04.25 ~ 2023.10.27'}
+        raffleImg={raffleImg}
+        price={'응모 금액 : 1000원'}
+        profileImg={ProfileImg}
+        profileName={'도티'}
+        content={
+          'jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구jgl여름쩌구 여름쩌구 저쩌구어쩌구 저쩌구'
+        }
+      />
     </>
   );
 }
